2389c806ed953e6ce1808b63c709bf0d887d34b7
sarahsteadman <fairyfaye4real@gmail.com>
2025-01-16 21:44:52 -0700

    App created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ContactListComponent } from './contacts/contact-list/contact-list.component';
+import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
+import { HeaderComponent } from './header/header.component';
+import { ContactItemComponent } from './contacts/contact-list/contact-item/contact-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContactsComponent,
+    ContactListComponent,
+    ContactDetailComponent,
+    ContactItemComponent
   ],
   imports: [
     BrowserModule
